4780b49ef9e06ece6db1e20f39a580a475050cf5
RalphSaridar55 <ralphsaridar@hotmail.com>
2021-11-29 21:00:40 +0200

    updated

--- balance.py
+++ balance.py
@@ -3,11 +3,14 @@ import telegram.ext
 from telegram import *
 import json
 import math
+import os
 import random
 
 api_key = "2121277949:AAGnsnht0fJVh_zrsybJdpuc9TgJn6YOo5c"
 chat_id = -1001775758804
+ADMINS = ["thebastardmak","cryptolima","watwatian","FaridFlintstone"]
 COIN_FLIP = ["Head","Tails"]
+WINNERS = 0
 ABOUT_US_MESSAGE = """** Lebanese DeFi **\n\nWelcome to the group
 
 We are working on building a community
@@ -16,6 +19,10 @@ La ne2dar kelna na3mol profits w nse3ed ba3ed at the end of the day
 
 From token whitelists to presales  to launch dates to even NFTs"""
 print("Bot running in tg")
+
+def notAllowed(update,context):
+	message = ["Not allowed habibo","Enssss","Jareba ba3ed marra barken btezbat"]
+	update.message.reply_text(message[random.randint(0,len(message)-1)])
 
 def start(update, context):
 	update.message.reply_text("Hello! Welcome to Lebanese DeFi!")
@@ -42,7 +49,8 @@ def myWallet(update, context):
 
 	res = page.json()
 	print("{:.18f}".format(float(res["result"])))
-	update.message.reply_text("**Your Balance:**\n"+str(float(res['result'])/math.pow(10,18))+" BNB")
+	sum = round(float(res['result'])/math.pow(10,18),2)
+	update.message.reply_text("**Your Balance:**\n"+str(sum)+" BNB")
 
 def yoda(update,context):
 
@@ -118,33 +126,82 @@ def balance(update, context):
 	page = requests.get("https://api.bscscan.com/api?module=account&action=balance&address="+address+"&apikey=T7YH7MHHQTPEB25EVTQH5Z7R5H1QK1KVXT")
 
 	res = page.json()
-	print("{:.18f}".format(float(res["result"])))
-	update.message.reply_text("**Community Balance:**\n"+str(float(res['result'])/math.pow(10,18))+" BNB")
+	update.message.reply_text("**Community Balance:**\n"+str("{0:.2f}".format(float(res['result'])/math.pow(10,18)))+" BNB")
 
 def callback():
 	print("test")
 
 def giveaway(update, context):
-	message = update.message["text"]
-	print("MESSAGE: ",message)
-	giveawayName = message.split(" ")[1]
-	winnersLn = int(message.split(" ")[3])
-	participantsLn = int(message.split(" ")[2])
-	print("Test")
-	button = [[InlineKeyboardButton("Join",callback_data="join_giveaway")]]
-	context.bot.send_message(chat_id=update.effective_chat.id, text="Giveaway "+giveawayName+"\n\n\nNum of Participants: "+str(participantsLn)+"\nNum of Winners: "+str(winnersLn),
-	 reply_markup=InlineKeyboardMarkup(button))
+	sender = update.message.from_user.username
+	print("SENDER:"+sender)
+	if(sender not in ADMINS):
+		notAllowed(update,context)
+	else:
+		global WINNERS
+		message = update.message["text"]
+		#print("MESSAGE: ",message)
+		giveawayName = message.split(" ")[1]
+		winnersLn = int(message.split(" ")[2])
+		WINNERS = winnersLn
+		#participantsLn = int(message.split(" ")[2])
+		#print("Test")
+		button = [[InlineKeyboardButton("Join",callback_data="join_giveaway"+"-"+update.message.from_user.first_name+"")]]
+		context.bot.send_message(chat_id=update.effective_chat.id, text="Giveaway "+giveawayName+"\n\n\nNumber of Winners: "+str(winnersLn),
+		reply_markup=InlineKeyboardMarkup(button))
 
 def MessageHandler(update, context):
 	message = update.message["text"]
 	if("Ass" in message or "ass" in message):
 		update.message.reply_text("Its ess for fuck's sake")
 
-def queryHandler(update, context):
-	query = update.callback_query.data
-	update.callback_query.answer()
+def stopGiveaway(update, context):
+	sender = update.message.from_user.username
+	print("SENDER:"+sender)
+	if(sender not in ADMINS):
+		notAllowed(update,context)
+	else:
 
-	# if "join_giveaway" in query:
+		global WINNERS
+		f = open("giveaway.txt", "r")
+		participants=[]
+		for i in f.readlines():
+			participants.append(i)
+		participants=participants[1::]
+		print(participants)	
+		try:
+			while True:
+				participants.remove("\n")
+		except ValueError:
+			pass
+		if(len(participants)<=WINNERS):
+			winners=""	
+			for i,n in enumerate(participants):
+				print("i: ",i)
+				winners+=n
+			update.message.reply_text("Congrats:\n\n"+winners)
+		else:
+			winners=""
+			for i in range(WINNERS):
+				choice = random.randint(0,participants.len())
+				winners+=participants[choice]
+			update.message.reply_text("Congrats:\n\n"+winners)
+			
+		f.close()
+		f  = open("giveaway.txt", "w")
+		f.write("participants:\n")
+
+def queryHandler(update, context):
+	user = update.callback_query.from_user.username
+	query = update.callback_query.data.split("-")
+	update.callback_query.answer()
+	if "join_giveaway" in query[0]:
+		print(user)
+		f = open("giveaway.txt","r")
+		lines = f.readlines()
+		if("@"+user not in lines):
+			fi = open("giveaway.txt", "a")
+			fi.write("\n@"+user)
+			fi.close()
 	# 	update.message.reply_text("Success")
 
 
@@ -165,6 +222,7 @@ disp.add_handler(telegram.ext.CommandHandler("myBalance",myWallet))
 disp.add_handler(telegram.ext.CommandHandler("yoda",yoda))
 disp.add_handler(telegram.ext.CommandHandler("badbot",badbot))
 disp.add_handler(telegram.ext.CommandHandler("give",giveaway))
+disp.add_handler(telegram.ext.CommandHandler("stopgive",stopGiveaway))
 disp.add_handler(telegram.ext.MessageHandler(telegram.ext.filters.Filters.text, MessageHandler))
 disp.add_handler(telegram.ext.CallbackQueryHandler(queryHandler))
 
