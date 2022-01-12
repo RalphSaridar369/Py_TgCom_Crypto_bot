58bda695e6d4cfac90f889ff32539cb0dd05cba3
AmongTribesmen <AmongTribesmen@github.com>
2022-01-12 19:49:41 +0000

    added bold text in About.py

--- data/About.py
+++ data/About.py
@@ -1,9 +1,8 @@
-dataAbout = ['''**What’s the difference between trading on binance and trading on a DEX?**
-
+dataAbout = ['''<b> What\'s the difference between trading on binance and trading on a DEX? </b>\n
 Binance is a CEX (centralized exchange) which means the company controls everything. They control liquidity pools, rules on the platform, and most importantly, your account and wallets. They own they keys to your wallets, not you, and they can freeze your account or assets for any reason. Not to mention that new releases (like the shitcoins we deal with) are not listed on a CEX immediately.
 
 To trade on a DEX (decentralized exchange), you use a non-custodial wallet, which means you and only you have the keys to it (like metamask or trustwallet). Liquidity pools on DEXes are usually added by investors, and regulated by smart contracts, which means prices cannot be manipulated easily. 
 
-It’s a common misconception that trading on a DEX can lead to scams or hacks, which is true, but if you’re careful enough about what links you click, which Dapps you connect to, and which projects you buy, chances are your assets are more secure than a CEX.''',
+It\'s a common misconception that trading on a DEX can lead to scams or hacks, which is true, but if you\'re careful enough about what links you click, which Dapps you connect to, and which projects you buy, chances are your assets are more secure than a CEX.''',
 
 'Chapter 2','Chapter 3']
