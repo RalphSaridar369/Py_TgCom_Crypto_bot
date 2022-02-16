e0c92abf6a70f44a46c35b30bf7ac4a7907e7578
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-02-16 17:50:19 +0200

    testing

--- functions/inlinequeryhandler.py
+++ functions/inlinequeryhandler.py
@@ -24,12 +24,12 @@ def InlineQueryHandler(update, context):
 			input_message_content=InputTextMessageContent(getHtmlUrl(),parse_mode=ParseMode.HTML),
 			description="Shows all the ongoing whitelists.",
 		),
-	# InlineQueryResultArticle(
-    #         id = str(uuid4()),
-	# 		title="Calendar",
-	# 		input_message_content=InputTextMessageContent(getHtmlCalUrl(),parse_mode=ParseMode.HTML),
-	# 		description="Shows calendar for the day"
-	# 	),
+	InlineQueryResultArticle(
+            id = str(uuid4()),
+			title="Calendar",
+			input_message_content=InputTextMessageContent(getHtmlCalUrl(),parse_mode=ParseMode.HTML),
+			description="Shows calendar for the day"
+		),
 	# InlineQueryResultArticle(
     #         id = str(uuid4()),
 	# 		title="Presales and Launches",
